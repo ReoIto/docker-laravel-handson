958f38cc7d47241675c3d72d3119e4d71af9ffaa
ReoIto <leo.0210.ito@gmail.com>
2021-04-29 14:51:17 +0900

    @include

---
+++ backend/resources/views/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<!-- Navigation -->
+<nav class="top-menu-container">
+    <div class="logo-header">
+        <a href="#">
+            <img src="{{ URL('storage/top_bg.jpg') }}" width="200px">
+            <!-- another way for images  -->
+            <!-- <img src="{{ asset('storage/top_bg.jpg') }}" width="200px"> -->
+        </a>
+    </div>
+
+    <ul>
+        <li>
+            <a href="">About</a>
+        </li>
+        <li>
+            <a href="">Portfolio</a>
+        </li>
+        <li>
+            <a href="">Contact</a>
+        </li>
+    </ul>
+</nav>
