a09c0425b19b1476231eb967ffbf9da2107c8778
ReoIto <leo.0210.ito@gmail.com>
2021-05-05 22:28:06 +0900

    Change the way of view

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
+
+@foreach($posts as $post)
+  {{ $post['title'] }}
+@endforeach
+
 @section('content')
 <h1>ALL VOCAB HERE!</h1>
 
-<div class="pt-10">
-  <a href="posts/create" class="border-b-2 pg-2 border-dotted italic text-gray-600">
+<div class="pt-10 mb-5">
+  <a href="posts/create" class="text-xl border-b-2 pg-2 border-dotted italic text-gray-600">
     ADD NEW VOCAB &rarr;
   </a>
 </div>
@@ -11,13 +16,22 @@
 @foreach($posts as $post)
   <div class="bg-gray-50 mb-5 shadow-xl p-8 rounded lg:w-64">
     <div class="float-right">
-      <a href="/posts/{{ $post->id }}/edit" class="border-b-2 pg-2 border-dotted italic text-green-500">
+      <a href="/posts//edit" class="border-b-2 mb-3 pg-2 border-dotted italic text-green-500">
         EDIT &rarr;
       </a>
+      <form action="/posts/" method="POST">
+        @csrf
+        @method('delete')
+        <button type="submit" class="border-b-2 pg-2 border-dotted italic text-red-500">
+          DELETE &rarr;
+        </button>
+      </form>
     </div>
-    <a href="/posts/{{ $post->id }}">
-      <p>{{ $post->title }}</p>
-      <p>{{ $post->meaning }}</p>
+    <a href="/posts/" class="italic">
+      <p class="mb-5"><span class="text-xl">Title</span><br>
+         <span></span></p>
+      <p><span class="text-xl">Meaning</span><br>
+        <span></span></p>
     </a>
   </div>
 @endforeach
